6dac37c000c3b3490887c77903edf0fafd81374a
ppoo0 <160346337+ppoo0@users.noreply.github.com>
2025-01-25 11:27:01 +0530

    Update main.py

--- devgagan/modules/main.py
+++ devgagan/modules/main.py
@@ -224,13 +224,13 @@ async def batch_link(_, message):
         return
 
  
-    join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
+    join_button = InlineKeyboardButton("Join Channel", url="https://t.me/neetumamvoca")
     keyboard = InlineKeyboardMarkup([[join_button]])
 
     
     pin_msg = await app.send_message(
         user_id,
-        "Batch process started ⚡\n__Processing: 0/{cl}__\n\n**__Powered by Team SPY__**",
+        "Batch process started ⚡\n__Processing: 0/{cl}__\n\n**__Powered by 𝙼𝚁 𝚁𝙰𝙹𝙿𝚄𝚃__**",
         reply_markup=keyboard
     )
     try:
@@ -274,7 +274,7 @@ async def batch_link(_, message):
                         msg = await app.send_message(message.chat.id, f"Processing...")
                         await process_and_upload_link(userbot, user_id, msg.id, link, 0, message)
                         await pin_msg.edit_text(
-                        f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by Team SPY__**",
+                        f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by 𝙼𝚁 𝚁𝙰𝙹𝙿𝚄𝚃__**",
                         reply_markup=keyboard
                         )
                 except Exception as e:
@@ -285,7 +285,7 @@ async def batch_link(_, message):
             await set_interval(user_id, interval_minutes=20)
             await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
             await pin_msg.edit_text(
-                        f"Batch process completed for {cl} messages enjoy 🌝\n\n**__Powered by Team SPY__**",
+                        f"Batch process completed for {cl} messages enjoy 🌝\n\n**__Powered by 𝙼𝚁 𝚁𝙰𝙹𝙿𝚄𝚃__**",
                         reply_markup=keyboard
             )
             return
@@ -322,7 +322,7 @@ async def batch_link(_, message):
                             msg = await app.send_message(message.chat.id, f"Processing...")
                             await process_and_upload_link(userbot, user_id, msg.id, link, 0, message)
                             await pin_msg.edit_text(
-                            f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by Team SPY__**",
+                            f"Batch process started ⚡\n__Processing: {i - cs + 1}/{cl}__\n\n**__Powered by 𝙼𝚁 𝚁𝙰𝙹𝙿𝚄𝚃__**",
                             reply_markup=keyboard
                             )
                     except Exception as e:
@@ -335,7 +335,7 @@ async def batch_link(_, message):
         await app.send_message(message.chat.id, "Batch completed successfully! 🎉")
         await set_interval(user_id, interval_minutes=20)
         await pin_msg.edit_text(
-                        f"Batch completed for {cl} messages ⚡\n\n**__Powered by Team SPY__**",
+                        f"Batch completed for {cl} messages ⚡\n\n**__Powered by 𝙼𝚁 𝚁𝙰𝙹𝙿𝚄𝚃__**",
                         reply_markup=keyboard
         )
     except FloodWait as fw:
