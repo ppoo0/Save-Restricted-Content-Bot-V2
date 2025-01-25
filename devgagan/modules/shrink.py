0d00f10172526ec932b4bbb0c58439316893f133
ppoo0 <160346337+ppoo0@users.noreply.github.com>
2025-01-25 11:21:45 +0530

    Update shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -56,9 +56,9 @@ async def token_handler(client, message):
         return
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")  # Callback for Help button
+        image_url = "https://graph.org/file/e453a252ac9bb3a92506a.jpg"
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/neetumamvoca")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/RAJPUT681830")  # Callback for Help button
         keyboard = InlineKeyboardMarkup([
             [join_button],  # First button
             [premium]   # Second button
