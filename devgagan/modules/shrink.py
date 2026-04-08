be88770ab0b422feaac6454644ac55dd4266c29e
PRADEEP RAJPUT <160346337+ppoo0@users.noreply.github.com>
2026-04-08 21:24:16 +0530

    Update image URL and button links in shrink.py

--- devgagan/modules/shrink.py
+++ devgagan/modules/shrink.py
@@ -73,9 +73,9 @@ async def token_handler(client, message):
     msg = await app.get_messages(chat_id, 796)
     user_id = message.chat.id
     if len(message.command) <= 1:
-        image_url = "https://i.postimg.cc/v8q8kGyz/startimg-1.jpg"
-        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/team_spy_pro")
-        premium = InlineKeyboardButton("Get Premium", url="https://t.me/kingofpatal")   
+        image_url = "https://i.ibb.co/4RrNwDMC/savev2.png"
+        join_button = InlineKeyboardButton("Join Channel", url="https://t.me/exams_materiel")
+        premium = InlineKeyboardButton("Get Premium", url="https://t.me/rajput681830")   
         keyboard = InlineKeyboardMarkup([
             [join_button],   
             [premium]    
