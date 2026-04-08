e66e45a542cb6746eb87c3a5e6ec19d827ef406c
PRADEEP RAJPUT <160346337+ppoo0@users.noreply.github.com>
2026-04-08 21:39:31 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -41,11 +41,11 @@ async def devggn_boot():
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
 👨‍💻 Author: Gagan
-🌐 GitHub: https://github.com/devgaganin/
-📬 Telegram: https://t.me/team_spy_pro
-▶️ YouTube: https://youtube.com/@dev_gagan
+🌐 GitHub: https://github.com/pradeep-rajput-dev/
+📬 Telegram: https://t.me/exams_materiel
+▶️ YouTube: https://www.youtube.com/@thinkwithpradeep
 🗓️ Created: 2025-01-11
-🔄 Last Modified: 2025-01-11
+🔄 Last Modified: 2026-04-08
 🛠️ Version: 2.0.5
 📜 License: MIT License
 ---------------------------------------------------
