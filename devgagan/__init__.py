4193cce60798d1ae17dbe9977d1d61e2a2aa7926
PRADEEP RAJPUT <160346337+ppoo0@users.noreply.github.com>
2026-04-22 12:58:12 +0530

    Update __init__.py

--- devgagan/__init__.py
+++ devgagan/__init__.py
@@ -1,24 +1,9 @@
-# ---------------------------------------------------
-# File Name: __init__.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import asyncio
 import logging
 import time
 from pyrogram import Client
 from pyrogram.enums import ParseMode 
 from config import API_ID, API_HASH, BOT_TOKEN, STRING, MONGO_DB, DEFAULT_SESSION
-from telethon.sync import TelegramClient
 from motor.motor_asyncio import AsyncIOMotorClient
 
 loop = asyncio.new_event_loop()
@@ -40,7 +25,10 @@ app = Client(
     parse_mode=ParseMode.MARKDOWN
 )
 
-sex = TelegramClient('sexrepo', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+# ❌ TELETHON DISABLED (FloodWait fix)
+sex = None
+telethon_client = None
+
 
 if STRING:
     pro = Client("ggbot", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
@@ -53,18 +41,14 @@ if DEFAULT_SESSION:
 else:
     userrbot = None
 
-telethon_client = TelegramClient('telethon_session', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
 # MongoDB setup
 tclient = AsyncIOMotorClient(MONGO_DB)
-tdb = tclient["telegram_bot"]  # Your database
-token = tdb["tokens"]  # Your tokens collection
+tdb = tclient["telegram_bot"]
+token = tdb["tokens"]
 
 async def create_ttl_index():
-    """Ensure the TTL index exists for the `tokens` collection."""
     await token.create_index("expires_at", expireAfterSeconds=0)
 
-# Run the TTL index creation when the bot starts
 async def setup_database():
     await create_ttl_index()
     print("MongoDB TTL index created.")
